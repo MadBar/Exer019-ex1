e80fa9279272fc3654a77a24d7cb512dc2b255a3
Madeleine Barwén <barwen@outlook.com>
2017-11-14 10:40:11 +0100

    3 Com: Add Meth in MRM.java. main: instansiated object of MRM

--- src/com/exer019_ex1/classes/MyRandomMethods.java
+++ src/com/exer019_ex1/classes/MyRandomMethods.java
@@ -1,5 +1,28 @@
 package com.exer019_ex1.classes;
 
 public class MyRandomMethods {
+	public int randomBetween1And10() {
+		int i = 0;
+		return i;
+	}
 
+	public int randomBetween11And20() {
+		int i = 0;
+		return i;
+	}
+
+	public int randomBetween21And30() {
+		int i = 0;
+		return i;
+	}
+
+	public int randomBetween31And40() {
+		int i = 0;
+		return i;
+	}
+
+	public int randomBetween41And50() {
+		int i = 0;
+		return i;
+	}
 }
--- src/com/exer019_ex1/main/Main.java
+++ src/com/exer019_ex1/main/Main.java
@@ -1,7 +1,10 @@
 package com.exer019_ex1.main;
 
+import com.exer019_ex1.classes.MyRandomMethods;
+
 public class Main {
 	public static void main(String[] args) {
+		MyRandomMethods mRM = new MyRandomMethods();
 
 	}
 }
