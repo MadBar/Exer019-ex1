72776cf3f65a78c3cc1e773de0ff54453c726c57
Madeleine Barwén <barwen@outlook.com>
2017-11-14 10:51:45 +0100

    3 com: Finished App

--- src/com/exer019_ex1/classes/MyRandomMethods.java
+++ src/com/exer019_ex1/classes/MyRandomMethods.java
@@ -1,28 +1,36 @@
 package com.exer019_ex1.classes;
 
+import java.util.Random;
+
 public class MyRandomMethods {
+	Random random = new Random();
+	
 	public int randomBetween1And10() {
-		int i = 0;
+		//Random random = new Random();
+
+		int i = random.nextInt(10) + 1;
 		return i;
 	}
 
 	public int randomBetween11And20() {
-		int i = 0;
+		//Random random = new Random();
+
+		int i = random.nextInt(10) + 11;
 		return i;
 	}
 
 	public int randomBetween21And30() {
-		int i = 0;
+		int i = random.nextInt(10) + 21;
 		return i;
 	}
 
 	public int randomBetween31And40() {
-		int i = 0;
+		int i = random.nextInt(10) + 31;
 		return i;
 	}
 
 	public int randomBetween41And50() {
-		int i = 0;
+		int i = random.nextInt(10) + 41;
 		return i;
 	}
 }
--- src/com/exer019_ex1/main/Main.java
+++ src/com/exer019_ex1/main/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		MyRandomMethods mRM = new MyRandomMethods();
 
+		System.out.println(mRM.randomBetween1And10());
+		System.out.println(mRM.randomBetween11And20());
+		System.out.println(mRM.randomBetween21And30());
+		System.out.println(mRM.randomBetween31And40());
+		System.out.println(mRM.randomBetween41And50());
 	}
 }
