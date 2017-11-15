a9360f16d4ff9be837d0431c9205fd4050d28ac8
Madeleine Barwén <barwen@outlook.com>
2017-11-15 10:11:59 +0100

    Commits broken code. Started new proj: Exerc019-ex1

---
+++ src/com/main/Main.java
@@ -0,0 +1,15 @@
+package com.main;
+
+import com.example.maven.Exerc019_ex1.MyRandomMethods;
+
+public class Main {
+	public static void main(String[] args) {
+		MyRandomMethods mRM = new MyRandomMethods();
+
+		System.out.println(mRM.randomBetween1And10());
+		System.out.println(mRM.randomBetween11And20());
+		System.out.println(mRM.randomBetween21And30());
+		System.out.println(mRM.randomBetween31And40());
+		System.out.println(mRM.randomBetween41And50());
+	}
+}
